17d7a0945fa32a5d1fba8379312a470485aa0516
kayaton <andrianov.vital@yandex.ru>
2021-10-10 00:23:47 +0300

    2les

--- app/src/main/java/com/androidstudy/lab1/MainActivity.kt
+++ app/src/main/java/com/androidstudy/lab1/MainActivity.kt
@@ -2,10 +2,39 @@ package com.androidstudy.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main1)
+        Log.i("onCreate", "Активити было создано")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("onStart", "Старт")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("onResume", "Приложение в процессе работы")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("onPause", "Был вызван ассистент")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("onStop", "Был открыт ассистент")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("onRestart", "Был осуществлен возврат к приложению")
+    }
+
 }
